087d3984fed31069240b286d4dab6d740cbd11b9
pritamcn <pritams@capitalnumbers.com>
2022-08-13 21:49:18 +0530

    first commit

---
+++ pdp/src/App.jsx
@@ -0,0 +1,20 @@
+import React,{Suspense} from "react";
+import ReactDOM from "react-dom";
+
+import "./index.scss";
+import Header from "home/Header";
+import Footer from "home/Footer";
+import SafeComponent from "./SafeComponent";
+const App = () => (
+  <div className=" text-3xl mx-auto max-w-6xl">
+    <SafeComponent>
+    <Header/>
+    </SafeComponent>
+  
+     <div className="my-10">
+        PDP Page Content
+     </div>
+    <Footer/>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
